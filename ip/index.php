73a40b75e30ee78f079ffe7c878e2b353c506345
kawaiizenbo <48113593+kawaiizenbo@users.noreply.github.com>
2022-08-10 13:36:34 -0700

    g

---
+++ ip/index.php
@@ -0,0 +1,28 @@
+<?php
+
+function getUserIpAddr(){
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+        //ip from share internet
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+        //ip pass from proxy
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }else{
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+
+$txt = "{\"ip\":\"".getUserIpAddr()."\",\"user-agent\":\"".$_SERVER['HTTP_USER_AGENT']."\"}\n";
+print($txt);
+
+$myfile = fopen("log.txt", "a") or die("Unable to open file!");
+fwrite($myfile, $txt);
+fclose($myfile);
+
+if ($_GET['redir_url'] != null)
+{
+    header('Location: '.$_GET['redir_url'].'');
+}
+
+?>
