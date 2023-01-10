2b91623946eb2791ebfc1cf4dea1abf422946cc5
kawaiizenbo <48113593+kawaiizenbo@users.noreply.github.com>
2023-01-10 15:36:14 -0700

    fixes

--- messageboard/getHTML.php
+++ messageboard/getHTML.php
@@ -8,7 +8,7 @@ foreach ($posts['messages'] as $post)
     $out .= "<div class='post' id='".$post['id']."'><details open>"
         ."<summary><a href='#".$post['id']."' style='color: palevioletred;'>".$post['id']."</a> <span style='color: mediumorchid;'>".$post['nickname']."</span> <span style='color: darkgrey;'>".$post['time']."</span></summary>";
     if ($post['image_url'] !== "") $out .= "<img src='".$post['image_url']."' alt='image'><br>";
-    $out .= "<span class='body'>".$post['body']."</span>"
+    $out .= "<span class='body'>".str_replace("\n", "<br>", $post['body'])."</span>"
         ."</details></div>";
 }
 
--- messageboard/index.php
+++ messageboard/index.php
@@ -1,5 +1,5 @@
 <?php include 'templateTop.php'?>
-<title>Messagboard</title>
+<title>messageboard</title>
 <h1>messageboard</h1>
 <center>
 <form class="center" action="post.php" method="post">
@@ -15,7 +15,7 @@
 			</tr>
 			<tr>
 				<td>
-                    <label for="body">body</label>
+                    <label for="body">body *</label>
                 </td>
 				<td>
                     <textarea id="body" rows="8" cols="40" name="body"></textarea>
@@ -31,6 +31,7 @@
 			</tr>
 		</tbody>
     </table>
+    <span>* required</span>
     <input type="submit" value="post">
     <input type="reset" value="reset">
     <input type="button" value="refresh" onclick="location.reload()" />
